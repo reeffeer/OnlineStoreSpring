ab0b14a24c15e5c7fe755386cc969712fda59633
reeffeer <reefshark@list.ru>
2020-12-16 19:12:16 +0300

    Added html and controller

---
+++ ProductRepository.java
@@ -0,0 +1,52 @@
+package online_store;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Component
+@Primary
+public class ProductRepository {
+
+    private List<Product> products;
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void deleteProduct(Product product) {
+        products.remove(product);
+    }
+
+    public List<Product> getProduct() {
+        return Collections.unmodifiableList(products);
+    }
+
+    public Product getProductById(int id) {
+        if (products.size() != 0) {
+            for (Product product : products) {
+                if (id == product.getId()) {
+                    System.out.println(product);
+                } else {
+                    throw new NoSuchElementException("Can't find the product with ID " + id + ".");
+                }
+            }
+        }
+        return null;
+    }
+
+    @PostConstruct
+    public void init() {
+        List<Product> products = new LinkedList<>();
+        int id = 0;
+        addProduct (new Product(id++, "boots", 2000));
+        addProduct(new Product(id++, "sheet", 150));
+        addProduct(new Product(id++, "book", 790));
+        addProduct(new Product(id++, "knife", 3450));
+    }
+}
---
+++ StoreApp.java
@@ -0,0 +1,10 @@
+package lesson_3;
+
+import org.springframework.boot.SpringApplication;
+
+public class StoreApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApp.class, args);
+    }
+}
